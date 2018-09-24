a334e047427b11dc6d6477fc6f692ec384e18f9b
Naelia <naeliaaquino58@gmail.com>
2018-09-24 08:35:06 -0300

    Lista de exercicio 05

--- estruturas/src/main/java/br/edu/ifce/estruturas/conjuntos/ConjuntoEspalhamento.java
+++ estruturas/src/main/java/br/edu/ifce/estruturas/conjuntos/ConjuntoEspalhamento.java
@@ -1,5 +1,6 @@
 package br.edu.ifce.estruturas.conjuntos;
 
+import java.lang.module.ModuleDescriptor.Builder;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
@@ -9,10 +10,11 @@ public class ConjuntoEspalhamento {
 	private ArrayList<LinkedList<String>>tabelaEspalhamento = 
 	new ArrayList<LinkedList<String>>();
 	
+	private int totalDeElementos = 0;
 	public ConjuntoEspalhamento()	{
 		
 		for(int i=0; i<26;i++) {
-			LinkedList<String> lista=new LinkedList<String>();
+			LinkedList<String> lista =new LinkedList<String>();
 			tabelaEspalhamento.add(lista);
 		}
 		
@@ -26,21 +28,60 @@ public class ConjuntoEspalhamento {
 			int indice = this.funcaoEspalhamento(palavra);
 			LinkedList<String> lista = this.tabelaEspalhamento.get(indice);
 			lista.add(palavra);
+			totalDeElementos++;
 		}
 	}
 		
 		
 	public void remove(String palavra) {
 		
+		if(this.contem(palavra)) {
+			int indice = this.funcaoEspalhamento(palavra);
+			LinkedList<String> lista = this.tabelaEspalhamento.get(indice);
+			lista.remove(palavra);
+			totalDeElementos--;
+		}
 	}
+	
 	public boolean contem(String palavra) {
-		return false;
+		int indice = this.funcaoEspalhamento(palavra);
+		LinkedList<String> lista = this.tabelaEspalhamento.get(indice);
+		
+		return lista.contains(palavra);
 	}
+	
 	public List<String> pegaTodasPalavras(){
-		return null;
+		List <String> L = new LinkedList<String>() ;
+		for(int i = 0;i<=25;i++) {
+			LinkedList<String> lista = this.tabelaEspalhamento.get(i);
+			L.addAll(lista);
+			
+		}
+		return L;
 		
 	}
 	public int tamanho() {
-		return 0;
+		return totalDeElementos;
 	}
+	
+	@Override
+	public String toString() {
+		
+		StringBuilder b = new StringBuilder();
+		
+		for(int i = 0; i< tabelaEspalhamento.size(); i++){
+			LinkedList<String> lista = this.tabelaEspalhamento.get(i);
+			
+			if(!lista.isEmpty()) {
+				String palavra = lista.toString();
+			
+				b.append("["+palavra.charAt(1)+"]-->"+palavra);
+				b.append(" \n");
+			}
+				
+		}
+		
+		return b.toString();
+	}
+	
 }
--- estruturas/src/test/java/br/edu/ifce/estruturas/conjuntos/TestaFuncaoEspalhamento.java
+++ estruturas/src/test/java/br/edu/ifce/estruturas/conjuntos/TestaFuncaoEspalhamento.java
@@ -4,9 +4,23 @@ public class TestaFuncaoEspalhamento {
 
 	public static void main(String[] args) {
 		ConjuntoEspalhamento ce1 = new ConjuntoEspalhamento();
-		System.out.print(ce1.funcaoEspalhamento("GUSTAVO"));
-		ce1.adiciona("gustavo");
-		System.out.println();
+	
+		ce1.adiciona("Hélio");
+		ce1.adiciona("Heitor");
+		ce1.adiciona("Humberto");
+		ce1.adiciona("Ilclemar");
+		ce1.adiciona("Jabesmar");
+		ce1.adiciona("Keroleen");
+		ce1.adiciona("Leomildo");
+		ce1.adiciona("Fologênio");
+		ce1.adiciona("Grivelda");
+		ce1.adiciona("Gladistone");
+		ce1.adiciona("Gêngis");
+		
+	
+		System.out.println(ce1.toString());
+		
+		
 	}
 
 }
